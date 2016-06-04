97335ce6fee6b1774436392d7cbfee332976d907
Umber Bane <jn.bluhm@gmx.de>
2016-06-04 23:56:46 +0200

    added links between index and about page

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Rango says "Hey there, partner!"')
+    return HttpResponse('''Rango says "Hey there, partner!"
+        <a href="/rango/about">About</a>''')
 
+def about(request):
+    return HttpResponse('''Rango says: "This is the about page!"
+        <a href="/rango">Go back</a>''')
