9647374bc6b0ff099e177d394cd34fe396777452
Umber Bane <jan.niklas.bluhm@gmail.com>
2016-06-09 01:23:51 +0200

    Chapter 7 done, added a few comments

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -4,9 +4,12 @@ from rango import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about', views.about, name='about'),
+    # below is for adding a category
     url(r'add_category/$', views.add_category, name='add_category'),
+    # below is for adding pages to a category
     url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$',
         views.add_page, name='add_page'),
+    # below is for showing a category
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
         views.show_category, name='show_category'),
 ]
